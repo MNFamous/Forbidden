4c57429e4886763e9c315f9b1c74afd9cda34a65
Mastess <mnu.2001@outlook.com>
2023-07-29 21:16:08 +0300

    Soft test movement

--- Assets/Scripts/move.cs
+++ Assets/Scripts/move.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class move : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float speed;
     void Start()
     {
         
@@ -13,17 +13,17 @@ public class move : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.W)){
-            this.transform.position += (new Vector3(0,0,1));
+        if (Input.GetKey(KeyCode.W)){
+            this.transform.position += (new Vector3(0,0,1)) * Time.deltaTime * speed;
         }
-        if (Input.GetKeyDown(KeyCode.A)){
-            this.transform.position += (new Vector3(-1,0,0));
+        if (Input.GetKey(KeyCode.A)){
+            this.transform.position += (new Vector3(-1,0,0)) * Time.deltaTime * speed;
         }
-        if (Input.GetKeyDown(KeyCode.D)){
-            this.transform.position += (new Vector3(1,0,0));
+        if (Input.GetKey(KeyCode.D)){
+            this.transform.position += (new Vector3(1,0,0)) * Time.deltaTime * speed; 
         }
-        if (Input.GetKeyDown(KeyCode.S)){
-            this.transform.position += new Vector3(0,0,-1);
+        if (Input.GetKey(KeyCode.S)){
+            this.transform.position += (new Vector3(0,0,-1)) * Time.deltaTime * speed;
         }
     }
 }
