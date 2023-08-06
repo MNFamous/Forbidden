2b78fe5e096427d2924e8c74ee20b2501f366969
Mastess <mnu.2001@outlook.com>
2023-08-06 18:12:05 +0300

    Camera modified, movement system fixed by Sax

--- Assets/Scripts/move.cs
+++ Assets/Scripts/move.cs
@@ -2,18 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class move : MonoBehaviour
+public class Move : MonoBehaviour
 {
     public float speed;
     public Vector3 rotation;
+    public Rigidbody rb;
 
-    void Update()
-    {
+    void Update() {
         rotation = WorldManager.Instance.worldRotation;
+    }
+    void FixedUpdate()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
 
-        this.transform.position += 
-            (Quaternion.AngleAxis(rotation.y, Vector3.up)
-            * new Vector3(Input.GetAxisRaw("Horizontal"),0,Input.GetAxisRaw("Vertical"))) 
-            * Time.deltaTime * speed;
+        Vector3 direction = new Vector3(horizontal, 0, vertical).normalized;
+        Vector3 angle = Quaternion.AngleAxis(rotation.y, Vector3.up) * direction;
+
+        rb.velocity = angle * speed;
+
+        //rb.velocity +=
+        //    (Quaternion.AngleAxis(rotation.y, Vector3.up)
+        //    * new Vector3(Input.GetAxisRaw("Horizontal"),0,Input.GetAxisRaw("Vertical"))) 
+        //    * Time.deltaTime * speed;
     }
 }
